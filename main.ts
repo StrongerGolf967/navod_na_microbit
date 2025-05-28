c5f8fa5dc208ed404f569c5a121898b7f7da6aa1
StrongerGolf967 <ma.mirko.ma@gmail.com>
2025-05-28 22:10:32 +0200

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         . # . # .
-        . . . . .
         . # . # .
         . . . . .
+        . # . # .
         # # # # #
         `)
 })
@@ -18,10 +18,10 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
+        . # . # .
+        . # . # .
         . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
+        # # # # #
         . . . . .
         `)
 })
